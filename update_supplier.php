9f0b8ddc76b5c954b8a7e9376e03e4cf205d2914
Dimas-Pratama-Purwadinata <54875915+Dimas-Pratama-Purwadinata@users.noreply.github.com>
2020-06-08 22:21:10 +0700

    Update update_supplier.php

--- update_supplier.php
+++ update_supplier.php
@@ -1,39 +1,21 @@
 <?php
 include_once("init.php");
-
 ?>
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <title>POSNIC - Update Supplier</title>
-
-    <!-- Stylesheets -->
-
     <link rel="stylesheet" href="css/style.css">
-
-    <!-- Optimize for mobile devices -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-    <!-- jQuery & JS files -->
     <?php include_once("tpl/common_js.php"); ?>
     <script src="js/script.js"></script>
     <script src="js/update_supplier.js"></script>
-   
 </head>
 <body>
-
-<!-- TOP BAR -->
 <?php include_once("tpl/top_bar.php"); ?>
-<!-- end top-bar -->
-
-
-<!-- HEADER -->
 <div id="header-with-tabs">
-
     <div class="page-full-width cf">
-
         <ul id="tabs" class="fl">
             <li><a href="dashboard.php" class="dashboard-tab">Dashboard</a></li>
             <li><a href="view_sales.php" class=" sales-tab">Sales</a></li>
@@ -44,10 +26,6 @@ include_once("init.php");
             <li><a href="view_payments.php" class="payment-tab">Payments / Outstandings</a></li>
             <li><a href="view_report.php" class="report-tab">Reports</a></li>
         </ul>
-        <!-- end tabs -->
-
-        <!-- Change this image to your own company's logo -->
-        <!-- The logo will automatically be resized to 30px height. -->
         <a href="#" id="company-branding-small" class="fr"><img src="<?php if (isset($_SESSION['logo'])) {
                 echo "upload/" . $_SESSION['logo'];
             } else {
@@ -55,41 +33,23 @@ include_once("init.php");
             } ?>" alt="Point of Sale"/></a>
 
     </div>
-    <!-- end full-width -->
-
 </div>
-<!-- end header -->
-
-
-<!-- MAIN CONTENT -->
 <div id="content">
-
     <div class="page-full-width cf">
-
         <div class="side-menu fl">
-
             <h3>Supplier Management</h3>
             <ul>
                 <li><a href="add_supplier.php">Add Supplier</a></li>
                 <li><a href="view_supplier.php">View Suppliers</a></li>
             </ul>
-
         </div>
-        <!-- end side-menu -->
-
         <div class="side-content fr">
-
             <div class="content-module">
-
                 <div class="content-module-heading cf">
-
                     <h3 class="fl">Update Supplier</h3>
                     <span class="fr expand-collapse-text">Click to collapse</span>
                     <span class="fr expand-collapse-text initial-expand">Click to expand</span>
-
                 </div>
-                <!-- end content-module-heading -->
-
                 <div class="content-module-main cf">
                     <form name="form1" method="post" id="form1" action="">
                         <p><strong>Add Stock </strong> - Add New ( Control + 3)</p>
@@ -103,20 +63,16 @@ include_once("init.php");
                                 $contact1 = trim(mysqli_real_escape_string($db->connection, $_POST['contact1']));
                                 $contact2 = trim(mysqli_real_escape_string($db->connection, $_POST['contact2']));
 
-
                                 if ($db->query("UPDATE supplier_details  SET supplier_name ='$name',supplier_address='$address',supplier_contact1='$contact1',supplier_contact2='$contact2' where id='$id'"))
                                     echo "<br><font color=green size=+1 > [ $name ] Supplier Details Updated!</font>";
                                 else
                                     echo "<br><font color=red size=+1 >Problem in Updation !</font>";
-
-
                             }
 
                             ?>
                             <?php
                             if (isset($_GET['sid']))
                                 $id = $_GET['sid'];
-
                             $line = $db->queryUniqueObject("SELECT * FROM supplier_details WHERE id=$id");
                             ?>
                             <form name="form1" method="post" id="form1" action="">
@@ -146,46 +102,21 @@ include_once("init.php");
                                                value="<?php echo $line->supplier_contact2; ?>"/></td>
                                 </tr>
 
-
                                 <tr>
                                     <td>
                                         &nbsp;
                                     </td>
                                     <td>
-                                        <input class="button round blue image-right ic-add text-upper" type="submit"
-                                               name="Submit" value="Save">
+                                        <input class="button round blue image-right ic-add text-upper" type="submit"name="Submit" value="Save">
                                         <b>(Control + S)</b>
                                     </td>
-                                    <td align="right"><input class="button round red   text-upper" type="reset"
-                                                             name="Reset" value="Reset"></td>
-
+                                    <td align="right"><input class="button round red   text-upper" type="reset" name="Reset" value="Reset"></td>
                                 </tr>
                         </table>
                     </form>
-
-
                 </div>
-                <!-- end content-module-main -->
-
-
             </div>
-            <!-- end content-module -->
-
-
         </div>
-        <!-- end full-width -->
-
     </div>
-    <!-- end content -->
-
-
-    <!-- FOOTER -->
-    <div id="footer">
-        <p>Any Queries email to <a href="mailto:sridhar.posnic@gmail.com?subject=Stock%20Management%20System">sridhar.posnic@gmail.com</a>.
-        </p>
-
-    </div>
-    <!-- end footer -->
-
 </body>
 </html>

53b65b9158019278a19c134766d65c27319f243e
Dimas-Pratama-Purwadinata <54875915+Dimas-Pratama-Purwadinata@users.noreply.github.com>
2020-06-08 21:55:01 +0700

    Update update_payment.php

--- update_payment.php
+++ update_payment.php
@@ -1,41 +1,24 @@
 <?php
 include_once("init.php");
-
 ?>
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <title>POSNIC - Update Payment</title>
-
-    <!-- Stylesheets -->
-
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="js/date_pic/date_input.css">
-
-    <!-- Optimize for mobile devices -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-    <!-- jQuery & JS files -->
     <?php include_once("tpl/common_js.php"); ?>
     <script src="js/date_pic/jquery.date_input.js"></script>
     <script src="js/script.js"></script>
     <script src="js/update_payment.js"></script>
-    
 </head>
 <body>
 
-<!-- TOP BAR -->
 <?php include_once("tpl/top_bar.php"); ?>
-<!-- end top-bar -->
-
-
-<!-- HEADER -->
 <div id="header-with-tabs">
-
     <div class="page-full-width cf">
-
         <ul id="tabs" class="fl">
             <li><a href="dashboard.php" class="dashboard-tab">Dashboard</a></li>
             <li><a href="view_sales.php" class="sales-tab">Sales</a></li>
@@ -46,30 +29,16 @@ include_once("init.php");
             <li><a href="view_payments.php" class="active-tab payment-tab">Payments / Outstandings</a></li>
             <li><a href="view_report.php" class="report-tab">Reports</a></li>
         </ul>
-        <!-- end tabs -->
-
-        <!-- Change this image to your own company's logo -->
-        <!-- The logo will automatically be resized to 30px height. -->
         <a href="#" id="company-branding-small" class="fr"><img src="<?php if (isset($_SESSION['logo'])) {
                 echo "upload/" . $_SESSION['logo'];
             } else {
                 echo "upload/posnic.png";
             } ?>" alt="Point of Sale"/></a>
-
     </div>
-    <!-- end full-width -->
-
 </div>
-<!-- end header -->
-
-
-<!-- MAIN CONTENT -->
 <div id="content">
-
     <div class="page-full-width cf">
-
         <div class="side-menu fl">
-
             <h3>Payment</h3>
             <ul>
                 <li><a href="view_payments.php">Payments</a></li>
@@ -94,8 +63,7 @@ include_once("init.php");
                     </tr>
                     <tr>
                         <td width="110"><strong><?php
-
-                                echo $db->queryUniqueValue("select sum(balance) FROM  stock_entries where count1=1 and type='entry'");
+                            echo $db->queryUniqueValue("select sum(balance) FROM  stock_entries where count1=1 and type='entry'");
                                 ?></strong></td>
                     </tr>
                     <tr>
@@ -142,24 +110,15 @@ include_once("init.php");
                 <br><br>
             </div>
         </div>
-        <!-- end side-menu -->
-
         <div class="side-content fr">
-
             <div class="content-module">
-
                 <div class="content-module-heading cf">
-
                     <h3 class="fl">Update Payment</h3>
                     <span class="fr expand-collapse-text">Click to collapse</span>
                     <span class="fr expand-collapse-text initial-expand">Click to expand</span>
-
                 </div>
-                <!-- end content-module-heading -->
-
                 <div class="content-module-main cf">
                     <form name="form1" method="post" id="form1" action="">
-
                         <table class="form" border="0" cellspacing="0" cellpadding="0">
                             <?php
                             if (isset($_POST['id'])) {
@@ -185,25 +144,20 @@ include_once("init.php");
                                     echo "<script>window.open('payment_receipt_print.php?sid=$max','myNewWinsr','width=620,height=800,toolbar=0,menubar=no,status=no,resizable=yes,location=no,directories=no');</script>";
                                 } else
                                     echo "<br><font color=red size=+1 >Problem in Updation !</font>";
-
-
                             }
-
                             ?>
                             <?php
                             if (isset($_GET['sid']))
                                 $id = $_GET['sid'];
-
                             $line = $db->queryUniqueObject("SELECT * FROM stock_sales WHERE transactionid='$id'");
                             ?>
                             <form name="form1" method="post" id="form1" action="">
-
                                 <input name="id" type="hidden" value="<?php echo $_GET['sid']; ?>">
                                 <tr>
                                     <td>&nbsp;</td>
                                     <td>Sales ID</td>
-                                    <td><input name="stock_id" type="text" readonly="readonly" readonly="readonly"
-                                               id="stockid" maxlength="200" class="round default-width-input"
+                                    <td><input name="stock_id" type="text" readonly="readonly" 
+                                               readonly="readonly" id="stockid" maxlength="200" class="round default-width-input" 
                                                value="<?php echo $line->transactionid; ?> "/>
                                     </td>
                                     <td>&nbsp;</td>
@@ -219,7 +173,6 @@ include_once("init.php");
                                                class="round default-width-input"
                                                value="<?php echo $line->subtotal; ?>"/></td>
                                 </tr>
-
                                 <tr>
                                     <td>&nbsp;</td>
                                     <td>Paid</td>
@@ -245,47 +198,22 @@ include_once("init.php");
                                                onkeyup="change_balance()" class="round default-width-input"
                                             /></td>
                                 </tr>
-
-
                                 <tr>
                                     <td>&nbsp;</td>
                                     <td>&nbsp;</td>
                                     <td>
-                                        <input class="button round blue image-right ic-add text-upper" type="submit"
-                                               name="Submit" value="Save">
+                                        <input class="button round blue image-right ic-add text-upper" type="submit" name="Submit" value="Save">
                                         (Control + S)
                                     </td>
-                                    <td align="right"><input class="button round red   text-upper" type="reset"
-                                                             name="Reset" value="Reset">
+                                    <td align="right"><input class="button round red   text-upper" type="reset"name="Reset" value="Reset">
                                     </td>
                                     <td>&nbsp;</td>
                                 </tr>
                         </table>
                     </form>
-
-
                 </div>
-                <!-- end content-module-main -->
-
-
             </div>
-            <!-- end content-module -->
-
-
         </div>
-        <!-- end full-width -->
-
     </div>
-    <!-- end content -->
-
-
-    <!-- FOOTER -->
-    <div id="footer">
-        <p>Any Queries email to <a href="mailto:sridhar.posnic@gmail.com?subject=Stock%20Management%20System">sridhar.posnic@gmail.com</a>.
-        </p>
-
-    </div>
-    <!-- end footer -->
-
 </body>
 </html>

a2d52050aeff423e424271571766738bf1fed5a1
Dimas-Pratama-Purwadinata <54875915+Dimas-Pratama-Purwadinata@users.noreply.github.com>
2020-06-08 22:22:21 +0700

    Update view_category.php

--- view_category.php
+++ view_category.php
@@ -2,38 +2,20 @@
 include_once("init.php");
 ?>
 <!DOCTYPE html>
-
 <html lang="en">
     <head>
         <meta charset="utf-8">
         <title>Pure Nature Shop - Jenis Produk</title>
-
-        <!-- Stylesheets -->
-        <!---->
         <link rel="stylesheet" href="css/style.css">
-
-        <!-- Optimize for mobile devices -->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-        <!-- jQuery & JS files -->
         <?php include_once("tpl/common_js.php"); ?>
         <script src="js/script.js"></script>
         <script src="js/view_category.js"></script>
-
-
     </head>
     <body>
-
-        <!-- TOP BAR -->
         <?php include_once("tpl/top_bar.php"); ?>
-        <!-- end top-bar -->
-
-
-        <!-- HEADER -->
         <div id="header-with-tabs">
-
             <div class="page-full-width cf">
-
               <ul id="tabs" class="fl">
                   <li><a href="dashboard.php" class=" dashboard-tab">Dashboard</a></li>
                   <li><a href="view_sales.php" class="sales-tab">Penjualan</a></li>
@@ -47,14 +29,9 @@ include_once("init.php");
           <a href="dashboard.php" id="company-branding-small" class="fr"><img src="images/s.png" alt=""></a>
           </div>
         </div>
-
-        <!-- MAIN CONTENT -->
         <div id="content">
-
             <div class="page-full-width cf">
-
                 <div class="side-menu fl">
-
                   <h3>Kelola Data Produk</h3>
                   <ul>
                       <li><a href="add_stock.php">Tambah Bibit Tanaman</a></li>
@@ -63,25 +40,13 @@ include_once("init.php");
                       <li><a href="view_category.php">Lihat Jenis Produk</a></li>
                       <li><a href="view_stock_availability.php">Lihat Stok</a></li>
                   </ul>
-
                 </div>
-                <!-- end side-menu -->
-
                 <div class="side-content fr">
-
                     <div class="content-module">
-
                         <div class="content-module-heading cf">
-
                           <h3 class="fl">Jenis Produk</h3>
-                          <!-- <span class="fr expand-collapse-text">Click to collapse</span>
-                          <span class="fr expand-collapse-text initial-expand">Click to expand</span> -->
                         </div>
-                        <!-- end content-module-heading -->
-
                         <div class="content-module-main cf">
-
-
                             <table>
                                 <form action="" method="post" name="search">
                                     <input name="searchtxt" type="text" class="round my_text_box" placeholder="Search">
@@ -111,8 +76,6 @@ include_once("init.php");
                                     <input name="dsubmit" type="button" value="hapus item terpilih"
                                            class="my_button round blue   text-upper" style="margin-left:5px;"
                                            onclick="return confirmDeleteSubmit()"/>
-
-
                                     <table>
                                         <?php
                                         $SQL = "SELECT * FROM  category_details ORDER BY id DESC";
@@ -121,20 +84,8 @@ include_once("init.php");
                                             $SQL = "SELECT * FROM  category_details WHERE category_name LIKE '%" . $_POST['searchtxt'] . "%' ORDER BY id DESC ";
                                         }
 
-                                        $tbl_name = "category_details";        //your table name
-                                        // How many adjacent pages should be shown on each side?
-
+                                        $tbl_name = "category_details";       
                                         $adjacents = 3;
-
-
-                                        /*
-
-                                          First get total number of rows in data table.
-
-                                          If you have a WHERE clause in your query, make sure you mirror it here.
-
-                                         */
-
                                         $query = "SELECT COUNT(*) as num FROM $tbl_name";
                                         if (isset($_POST['Search']) AND trim($_POST['searchtxt']) != "") {
 
@@ -145,79 +96,37 @@ include_once("init.php");
                                         $total_pages = mysqli_fetch_array(mysqli_query($db->connection, $query));
 
                                         $total_pages = $total_pages['num'];
-
-
-                                        /* Setup vars for query. */
-
-                                        $targetpage = "view_category.php";    //your file name  (the name of this file)
-
-                                        $limit = 10;                                //how many items to show per page
+                                        $targetpage = "view_category.php";    
+                                        $limit = 10;                                
                                         if (isset($_GET['limit']) && is_numeric($_GET['limit'])) {
                                             $limit = $_GET['limit'];
                                             $_GET['limit'] = 10;
                                         }
-
                                         $page = isset($_GET['page']) ? $_GET['page'] : 0;
-
-
                                         if ($page)
-                                            $start = ($page - 1) * $limit;            //first item to display on this page
+                                            $start = ($page - 1) * $limit;          
                                         else
-                                            $start = 0;                                //if no page var is given, set start to 0
-
-
-                                            /* Get data. */
-
+                                            $start = 0;                                
                                         $sql = "SELECT * FROM category_details ORDER BY id DESC LIMIT $start, $limit  ";
                                         if (isset($_POST['Search']) AND trim($_POST['searchtxt']) != "") {
 
                                             $sql = "SELECT * FROM  category_details WHERE category_name LIKE '%" . $_POST['searchtxt'] . "%' ORDER BY id DESC LIMIT $start, $limit";
                                         }
-
-
                                         $result = mysqli_query($db->connection, $sql);
-
-
-                                        /* Setup page vars for display. */
-
                                         if ($page == 0)
-                                            $page = 1;                    //if no page var is given, default to 1.
-
-                                        $prev = $page - 1;                            //previous page is page - 1
-
-                                        $next = $page + 1;                            //next page is page + 1
-
-                                        $lastpage = ceil($total_pages / $limit);        //lastpage is = total pages / items per page, rounded up.
-
-                                        $lpm1 = $lastpage - 1;                        //last page minus 1
-
-
-                                        /*
-
-                                          Now we apply our rules and draw the pagination object.
-
-                                          We're actually saving the code to a variable in case we want to draw it more than once.
-
-                                         */
-
+                                            $page = 1;                    
+                                        $prev = $page - 1;                          
+                                        $next = $page + 1;                          
+                                        $lastpage = ceil($total_pages / $limit);       
+                                        $lpm1 = $lastpage - 1;                        
                                         $pagination = "";
-
                                         if ($lastpage > 1) {
-
                                             $pagination .= "<div >";
-
-                                            //previous button
-
                                             if ($page > 1)
                                                 $pagination .= "<a href=\"view_category.php?page=$prev&limit=$limit\" class=my_pagination >Previous</a>";
                                             else
                                                 $pagination .= "<span class=my_pagination>Previous</span>";
-
-
-                                            //pages
-
-                                            if ($lastpage < 7 + ($adjacents * 2)) {    //not enough pages to bother breaking it up
-
+                                            if ($lastpage < 7 + ($adjacents * 2)) {   
                                                 for ($counter = 1; $counter <= $lastpage; $counter++) {
 
                                                     if ($counter == $page)
@@ -225,33 +134,23 @@ include_once("init.php");
                                                     else
                                                         $pagination .= "<a href=\"view_category.php?page=$counter&limit=$limit\" class=my_pagination>$counter</a>";
                                                 }
-                                            } elseif ($lastpage > 5 + ($adjacents * 2)) {    //enough pages to hide some
-
-                                                //close to beginning; only hide later pages
-
+                                            } elseif ($lastpage > 5 + ($adjacents * 2)) {
                                                 if ($page < 1 + ($adjacents * 2)) {
-
                                                     for ($counter = 1; $counter < 4 + ($adjacents * 2); $counter++) {
-
                                                         if ($counter == $page)
                                                             $pagination .= "<span class=my_pagination>$counter</span>";
                                                         else
                                                             $pagination .= "<a href=\"view_category.php?page=$counter&limit=$limit\" class=my_pagination>$counter</a>";
                                                     }
-
                                                     $pagination .= "...";
 
                                                     $pagination .= "<a href=\"view_category.php?page=$lpm1&limit=$limit\" class=my_pagination>$lpm1</a>";
 
                                                     $pagination .= "<a href=\"view_category.php?page=$lastpage&limit=$limit\" class=my_pagination>$lastpage</a>";
-                                                } //in middle; hide some front and some back
-
+                                                } 
                                                 elseif ($lastpage - ($adjacents * 2) > $page && $page > ($adjacents * 2)) {
-
                                                     $pagination .= "<a href=\"view_category.php?page=1&limit=$limit\" class=my_pagination>1</a>";
-
                                                     $pagination .= "<a href=\"view_category.php?page=2&limit=$limit\" class=my_pagination>2</a>";
-
                                                     $pagination .= "...";
 
                                                     for ($counter = $page - $adjacents; $counter <= $page + $adjacents; $counter++) {
@@ -267,8 +166,7 @@ include_once("init.php");
                                                     $pagination .= "<a href=\"view_category.php?page=$lpm1&limit=$limit\" class=my_pagination>$lpm1</a>";
 
                                                     $pagination .= "<a href=\"view_category.php?page=$lastpage&limit=$limit\" class=my_pagination>$lastpage</a>";
-                                                } //close to end; only hide early pages
-
+                                                } 
                                                 else {
 
                                                     $pagination .= "<a href=\"$view_category.php?page=1&limit=$limit\" class=my_pagination>1</a>";
@@ -286,10 +184,6 @@ include_once("init.php");
                                                     }
                                                 }
                                             }
-
-
-                                            //next button
-
                                             if ($page < $counter - 1)
                                                 $pagination .= "<a href=\"view_category.php?page=$next&limit=$limit\" class=my_pagination>Next</a>";
                                             else
@@ -308,7 +202,6 @@ include_once("init.php");
                                         </tr>
 
                                         <?php
-                                        //count no of recards
                                         $co = 0;
                                         $co1 = 0;
                                         $s = mysqli_query($db->connection, "select * from category_details");
@@ -343,23 +236,12 @@ include_once("init.php");
                                                            id="check_box"/></td>
 
                                             </tr>
-    <?php $i++;
-}
-?>
+    <?php $i++;}?>
                                         <table>
-
-
                                         </table>
                                     </table>
                                 </form>
-
-
                         </div>
                     </div>
-                    <div id="footer">
-
-                    </div>
-                    <!-- end footer -->
-
                     </body>
                     </html>

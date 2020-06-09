90ca76d181e5347af03a14183f3b163ff8118654
Dimas-Pratama-Purwadinata <54875915+Dimas-Pratama-Purwadinata@users.noreply.github.com>
2020-06-09 21:06:02 +0700

    Update update_stock.php

--- update_stock.php
+++ update_stock.php
@@ -5,7 +5,7 @@ include_once("init.php");
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>POSNIC - Update Pemasok</title>
+    <title>Pure Nature Shop - Update Pemasok</title>
     <link rel="stylesheet" href="css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <?php include_once("tpl/common_js.php"); ?>

b8599702e6f80ef21ffa4a66ae58acb023151347
Dimas-Pratama-Purwadinata <54875915+Dimas-Pratama-Purwadinata@users.noreply.github.com>
2020-06-11 08:40:46 +0700

    Update view_supplier.php

--- view_supplier.php
+++ view_supplier.php
@@ -261,5 +261,6 @@ include_once("init.php");
 
                         </div>
                     </div>
+		    <?php include_once("tpl/footer.php"); ?>
                     </body>
                     </html>

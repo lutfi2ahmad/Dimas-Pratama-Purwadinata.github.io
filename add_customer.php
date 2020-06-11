cf9ce35b265913158b690235cc93d8e50ec0c09e
Dimas-Pratama-Purwadinata <54875915+Dimas-Pratama-Purwadinata@users.noreply.github.com>
2020-06-11 20:57:51 +0700

    Update add_customer.php

--- add_customer.php
+++ add_customer.php
@@ -31,7 +31,7 @@ include_once("init.php");?>
 </div>
 <div id="content">
     <div class="page-full-width cf">
-        <div class="side-menu fl">
+        <div class="side-menu fr">
             <h3>kelola Data Pelanggan</h3>
             <ul>
                 <li><a href="add_customer.php">Tambah Pelanggan</a></li>
